300a52bd243d844c716d2f10f24d905f3caf2578
NiamhGrimesMcDonough <niamhaerospace@proton.me>
2026-06-17 16:11:40 +0100

    Implemented Civilizations that expand approaching the speed of light, They do not yet interact with other civilizations.

--- civ.py
+++ civ.py
@@ -1,24 +1,27 @@
 import pygame
+import random as rn
 alive = pygame.sprite.Group()
+CivColours = ["Purple", "Green", "Orange", "Teal", "Cyan", "Magenta", "Yellow", "White", "Blue", "Light Blue", "Red", "Pink"]
 def spawnCiv(xy, NoOfCivs):
     print("Civilization number " + str(NoOfCivs) + " Spawned at " + str(xy))
-    civilization = Civ("purple", 20 ,20, xy)
+    civilization = Civ(rn.choice(CivColours), xy, 10, 0.1)
     
 
-    alive.add(civilization)
-
-    pygame.display.flip()
-
-def handleCiv(screen):
-    alive.update()    
-    alive.draw(screen) 
-    pygame.display.flip()       
-
-
+    alive.add(civilization)      
 class Civ(pygame.sprite.Sprite):
-    def __init__(self, colour, height, width, centre):
-        super().__init__()
+    def __init__(self, colour, centre, radius, tech):
+        self.tech = tech
+        self.radius = radius
+        self.colour = colour
+        self.centre = centre
+        pygame.sprite.Sprite.__init__(self)
+        
+    def Advance(self):
+        self.tech += (1 - self.tech) * 0.01
+        self.radius += self.tech
+        print(f"tech={self.tech:.6f}, radius={self.radius:.6f}")
+        surface = pygame.display.get_surface()
+        pygame.draw.circle(surface, self.colour, self.centre, self.radius)
+    
 
-        self.image = pygame.Surface([width, height])
 
-        pygame.draw.circle(self.image, colour, centre, width)
--- main.py
+++ main.py
@@ -9,25 +9,27 @@ import civ
 pygame.init()
 
 screen = pygame.display.set_mode((1000, 1000))
-
 pygame.display.set_caption("Universe Civ Sim")
 clock = pygame.time.Clock()
-civ.handleCiv
-clock.tick(60)
-
 WindowSize = pygame.display.get_window_size()
 WindowHeight = WindowSize[0]
 print(WindowHeight)
 WindowWidth = WindowSize[1]
 
 GenerateStars(rn.randrange(config.MinStar, config.MaxStar), screen, WindowHeight, WindowWidth, 0)
-pygame.display.flip()
 running = True
+ADVANCE_EVENT = pygame.USEREVENT + 1
+pygame.time.set_timer(ADVANCE_EVENT, 100)
 while running:
     for event in pygame.event.get():
+        print(event)
+        if event.type == ADVANCE_EVENT:
+            print("Advance Called!")
+            for civilization in civ.alive:
+                civilization.Advance()
         if event.type == pygame.QUIT:
             running = False
+    pygame.display.flip()
+    clock.tick(60)
 
-pygame.quit
-
-print("Generating Stars")
+pygame.quit()
